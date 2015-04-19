bac9da6ba1879484e8b77a1ef7e826c27ab81abb
gbisht <gitas.bisht@gmail.com>
2015-04-19 14:11:12 -0700

    -Add seeds for the user, questions

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+user = FactoryGirl.create :user, email: 'admin@realdeminas.com'
+
+questions = %w{ question1 question2 question3 question4 question5 question6 question7 question8 question9
+question10  }
+
+first_child_questions = %w{ child_question_1 child_question_2 child_question_3 child_question_4 child_question_5 }
+
+second_child_questions = %w{ second_child_question_1 second_child_question_2 second_child_question_3 second_child_question_4 second_child_question_5 }
+
+
+questions.each do |question|
+	question_record = FactoryGirl.create :question, user: user, content: question
+end
+
+questions = Question.roots
+
+first_child_questions.each do |child_question|
+	child_record = FactoryGirl.create :question, user: user, content: child_question
+	child_record.move_to_child_of(questions.sample)
+end
+
+child_questions  =  Question.where depth: 1
+
+second_child_questions.each do |child_question|
+	child_record = FactoryGirl.create :question, user: user, content: child_question
+	child_record.move_to_child_of(child_questions.sample)
+end
+
+
+
