e9cb4adbcea6deae320f80f8800cfe386a898a5a
gbisht <gitas.bisht@gmail.com>
2015-05-04 21:52:51 -0700

    added validation on contact model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,6 @@
 class Contact < ActiveRecord::Base
   belongs_to :survey
+  validates :email, format: { with: /.+@.+\..+/i, message: "Invalid email ID" }, allow_nil: true
+  validates :phone_number, format: { with: /\d{10}/, message: "bad format" }, allow_nil: true
+  validates :survey_id, presence: true
 end
