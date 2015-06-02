e76b25ccabe423ddeb101caba2400d3736da687f
Geeta Bisht <gitas.bisht@gmail.com>
2015-06-01 21:52:55 -0700

    device path and after sign up path

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,6 +2,14 @@ class RegistrationsController < Devise::RegistrationsController
 
   private
 
+  def after_sign_up_path_for(resource)
+      dashboard_index_path(resource)
+  end
+
+  # def after_update_path_for(resource)
+  #   signed_in_root_path(resource)
+  # end
+
   def sign_up_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
