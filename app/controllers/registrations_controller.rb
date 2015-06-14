5280f63699e9f633f6f02c91297505cc8237ca22
Geeta Bisht <gitas.bisht@gmail.com>
2015-06-14 12:53:08 -0700

    changed the routes file

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,11 +2,6 @@ class RegistrationsController < Devise::RegistrationsController
 
   private
 
-  def after_sign_up_path_for(resource)
-    # TODO this is not properlyu working
-    dashboard_index_path(resource)
-  end
-
   def sign_up_params
     params.require(:user).permit(:question_type, :name, :email, :password, :password_confirmation)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'registrations' }
 
-  devise_scope :user do
-    get '/login', to: 'devise/sessions#new'
-    delete '/logout', to: 'devise/sessions#destroy'
-  end
-
   get ':user_name/', to: 'surveys#index'
   post ':user_name/', to: 'surveys#create'
   post ':user_name/comment', to: 'surveys#comment_and_contact_info'
 
   resources :dashboard, only: [:index, :show]
-
-  devise_scope :user do
-    root to: "devise/registrations#new"
-  end
 end
